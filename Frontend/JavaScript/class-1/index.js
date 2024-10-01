17156a26c155f5f1e2156cea60b8dfd7476bef85
SusmitaSahani20 <susmitasahani05@gmail.com>
2024-10-01 13:04:39 +0530

    USBM

--- Frontend/JavaScript/class-1/index.js
+++ Frontend/JavaScript/class-1/index.js
@@ -1,1 +1,16 @@
-console.log("Hey I am Susmita");
+var name="Susmita";
+let age=25;
+const city="Bhubaneswar";
+console.log(address);
+
+var address="Bhubaneswar";
+
+console.log(address);
+
+//null
+//let user;
+
+let user=null;
+
+console.log(typeof null);
+console.log(user);
