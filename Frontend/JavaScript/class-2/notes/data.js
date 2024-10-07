fc4c1f157b0f95474b74c0333933502c06c98226
SusmitaSahani20 <susmitasahani05@gmail.com>
2024-10-07 11:15:24 +0530

    Addition,Substraction,Multiplication,Division

---
+++ Frontend/JavaScript/class-2/notes/data.js
@@ -0,0 +1,30 @@
+//undefined
+let data;
+console.log(typeof data);//undefined
+
+//Number
+
+let num1=50;
+let deccimal_no=1.0001;
+let age=21;
+let pi=3.14;
+let roll_no=146;
+
+console.log(typeof num1);
+
+//Addition,substraction,multiplication and division
+let num2=100;
+let num3=125;
+let addition=num2+num3;
+let substraction=num2-num3;
+let multiplication=num2*num3;
+let division=num2/num3;
+
+console.log(addition);
+console.log(substraction);
+console.log(multiplication);
+console.log(division);
+
+
+
+
