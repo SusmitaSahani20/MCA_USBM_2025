fa885b96ddca319764bfca28c91a5aff9adbb710
SusmitaSahani20 <susmitasahani05@gmail.com>
2024-10-07 12:10:36 +0530

    Template

--- Frontend/JavaScript/class-2/notes/data.js
+++ Frontend/JavaScript/class-2/notes/data.js
@@ -1,30 +1,40 @@
 //undefined
-let data;
-console.log(typeof data);//undefined
+//let data;
+//console.log(typeof data);//undefined
 
 //Number
 
-let num1=50;
-let deccimal_no=1.0001;
-let age=21;
-let pi=3.14;
-let roll_no=146;
+//let num1=50;
+//let deccimal_no=1.0001;
+//let age=21;
+//let pi=3.14;
+//let roll_no=146;
 
-console.log(typeof num1);
+//console.log(typeof num1);
 
 //Addition,substraction,multiplication and division
-let num2=100;
-let num3=125;
-let addition=num2+num3;
-let substraction=num2-num3;
-let multiplication=num2*num3;
-let division=num2/num3;
+//let num2=100;
+//let num3=125;
+//let addition=num2+num3;
+//let substraction=num2-num3;
+//let multiplication=num2*num3;
+//let division=num2/num3;
 
-console.log(addition);
-console.log(substraction);
-console.log(multiplication);
-console.log(division);
+//console.log(addition);
+//console.log(substraction);
+//console.log(multiplication);
+//console.log(division);
 
 
+//Template literal:Backtik
+let name="Susmita";
+let city="kendrapara";
+let age=21;
+let regd_no=2305274146;
+let college="USBM";
+let batch="MCA 2nd Year";
+
+console.log(`My name is: ${name}, age is:${age}, city is: ${city}, Registration no is:${regd_no}, college is:${college}, Batch is :${batch}`);
+console.log(city.length);
 
 
