bc4e21c714b2b0b1c343df9c7f07f8c48752fd01
SusmitaSahani20 <susmitasahani05@gmail.com>
2024-10-08 11:49:17 +0530

    Task

---
+++ Frontend/JavaScript/class-3/notes/math.js
@@ -0,0 +1,69 @@
+//Mathematical Operator
+/*let num1=5;
+let num2=3;
+let result=num1+num2;  //Add
+let sub=num1-num2;
+let mul=num1*num2;
+let div=num1/num2;*/
+
+//Modulo operator
+ /*let num=29;
+ let odd=num%2;
+ console.log(odd);
+
+ //Exponentiation Operator
+ let sqr=2**2;
+ let cube=2**3;
+ console.log(sqr);
+ console.log(cube);*/
+
+ //Relational operator
+ //To compare two values
+ //`!` -->Not opearator
+/* console.log(10>5);//Boolean
+ console.log(10<5);//Boolean
+ console.log(5>5);//Boolean
+ console.log(5>=5);//Boolean
+ console.log(10> !5);//Boolean
+ console.log(!5);//Boolean*/
+
+ //Equality operator
+//`==` :Value check
+//`===`: value and data type check
+/*console.log(5==5);
+ console.log(5==6);
+ console.log(5=="5");
+ console.log(5==="5");
+ console.log(5!="5");
+ console.log(5!=="5");*/
+
+ //Conditional
+ //if (condition) {
+//true code to be executed here 
+//}
+
+
+//Falsy Value :
+//false , 0, -0, "",``,'',null,undefined,NaN
+/*if (-0.0512) {
+    console.log("It is true");
+}
+if(-100){
+    console.log("It is true");
+}*/
+
+//Task
+//Given total_bill,discount_start_price if you satisfy the condition then print discount
+//Available otherwise print no discount
+
+let total_bill=1000;
+let discount_start_price=500;
+if (total_bill>discount_start_price){
+    console.log("Discount available");
+
+}
+else{
+    console.log("No discount");
+}
+
+
